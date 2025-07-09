6319a1817419c9ac2e0195c1150def715433193c
Mohamed Tharwat <mthawrat@example.com>
2025-07-09 19:14:35 +0300

    update

--- src/components/mainPage/AzkarSection.jsx
+++ src/components/mainPage/AzkarSection.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../../App.css'
 //Icons
-import beads from '../Azkar/Icons/beads.png'
-import Day from '../Azkar/Icons/day-mode.png'
-import night from '../Azkar/Icons/night.png'
-import shalat from '../Azkar/Icons/shalat.png'
-import sleep from '../Azkar/Icons/sleep.png'
-import wakeup from '../Azkar/Icons/wake-up.png'
+// import beads from '/beads.png'
+// import Day from 'day-mode.png'
+// import night from '/night.png'
+// import shalat from '/shalat.png'
+// import sleep from '/sleep.png'
+// import wakeup from '/wake-up.png'
 // components
 import AzkarContainer from '../Azkar/AzkarContainer'
 
@@ -14,12 +14,12 @@ function AzkarSection() {
     return (
         <div className="w-full flex justify-center items-center p-12 rounded-md bg-[#785391]">
             <div className='grid grid-cols-2 md:grid-cols-3 gap-20'>
-                <AzkarContainer to={'/MorningAzkar'} title={"اذكار الصباح"} icon={Day}/>
-                <AzkarContainer to={'/NightAzkar'} title={"اذكار المساء"} icon={night}/>
-                <AzkarContainer to={'/PrayerAzkar'} title={"اذكار بعد الصلاة"} icon={shalat}/>
-                <AzkarContainer to={'/SleepingAzkar'} title={"اذكار قبل النوم"} icon={sleep}/>
-                <AzkarContainer to={'/WakeupAzkar'} title={"اذكار الاستيقاظ"} icon={wakeup}/>
-                <AzkarContainer to={'/Tasbeeh'} title={"تسابيح"} icon={beads}/>
+                <AzkarContainer to={'/MorningAzkar'} title={"اذكار الصباح"} icon={'/day-mode.png'}/>
+                <AzkarContainer to={'/NightAzkar'} title={"اذكار المساء"} icon={'/night.png'}/>
+                <AzkarContainer to={'/PrayerAzkar'} title={"اذكار بعد الصلاة"} icon={'/shalat.png'}/>
+                <AzkarContainer to={'/SleepingAzkar'} title={"اذكار قبل النوم"} icon={'/sleep.png'}/>
+                <AzkarContainer to={'/WakeupAzkar'} title={"اذكار الاستيقاظ"} icon={'/wake-up.png'}/>
+                <AzkarContainer to={'/Tasbeeh'} title={"تسابيح"} icon={'/beads.png'}/>
 
             </div>
         </div>
--- src/components/mainPage/QuranSection.jsx
+++ src/components/mainPage/QuranSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Koran from '../Azkar/Icons/Koran.png'
+// import Koran from '../Azkar/Icons/Koran.png'
 import { Link } from 'react-router-dom'
 
 function QuranSection() {
@@ -8,7 +8,7 @@ function QuranSection() {
         <div className="w-full h-auto p-8 rounded-md bg-gray-200 shadow-md overflow-hidden flex flex-col items-center">
             <div
                 className="w-[200px] h-[200px] bg-cover bg-center"
-                style={{ backgroundImage: `url(${Koran})` }}
+                style={{ backgroundImage: `url(${'/koran.png'})` }}
             ></div>
             <div className="p-2 w-full text-center">
                 <p className='max-w-[700px] mx-auto mt-6'>القرآن الكريم هو كتاب الله الذي أنزل هداية ورحمة للعالمين. في هذا القسم يمكنك قراءة القرآن كاملًا، والاستماع إلى تلاوات خاشعة، والبحث في السور والآيات بسهولة تامة.</p>
