2ae1ce4ada396ef5892cc3277f924ba88d110a58
Mohamed Tharwat <mthawrat@example.com>
2025-07-11 17:14:16 +0300

    update

--- src/components/mainPage/mainContent.jsx
+++ src/components/mainPage/mainContent.jsx
@@ -29,7 +29,8 @@ export default function MainPage() {
             Fajr={`${prayerTime.Fajr}:00`}
             Dhuhr={`${prayerTime.Dhuhr}:00`}
             Asr={`${prayerTime.Asr}:00`}
-            Maghrib={`${prayerTime.Maghrib}:00`}
+            Maghrib="05:15"
+            // Maghrib={`${prayerTime.Maghrib}:00`}
             Isha={`${prayerTime.Isha}:00`}
         />
         <Header loading={loading} TimeZone={TimeZone} />
